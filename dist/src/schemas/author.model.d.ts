815dfb0d29a392dd7a16a2ba9e74fd72ff28c95d
hungnv0902 <hungnv0902@gmail.com>
2022-06-28 14:30:38 +0700

    Add file

---
+++ dist/src/schemas/author.model.d.ts
@@ -0,0 +1,14 @@
+/// <reference types="mongoose/types/aggregate" />
+/// <reference types="mongoose/types/connection" />
+/// <reference types="mongoose/types/cursor" />
+/// <reference types="mongoose/types/document" />
+/// <reference types="mongoose/types/error" />
+/// <reference types="mongoose/types/mongooseoptions" />
+/// <reference types="mongoose/types/pipelinestage" />
+/// <reference types="mongoose/types/schemaoptions" />
+/// <reference types="mongoose" />
+interface IAuthor {
+    name: string;
+}
+declare const Author: import("mongoose").Model<IAuthor, {}, {}, {}>;
+export { Author };
---
+++ index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+import bookRoutes from './src/router/book.router';
+
+const app = express();
+app.set("view engine", "ejs");
+app.set('views', './src/views');
+
+const DB_URL = `mongodb://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`;
+console.log(DB_URL, "DB_URL")
+mongoose.connect(DB_URL)
+.then(() => console.log('DB Connected!'))
+.catch(error => console.log('DB connection error:', error.message));
+app.use(bodyParser.json());
+
+app.use('/book', bookRoutes);
+app.listen(process.env.PORT || 3000, () => {
+  console.log("App running on port: " + 3000)
+})
